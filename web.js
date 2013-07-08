6fb92b31f07b3a7f948bd051defd711421dbed52
BigDickHead <gg3be0@gmail.com>
2013-07-08 10:22:29 -0400

    Update web.js/index.html

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var fs = require('fs');
+var content = fs.readFileSync('index.html', 'utf-8');
+var buffer = new Buffer(content, 'utf-8');
+
+//console.log(content);
+//console.log(buffer.toString());
+//return;
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
